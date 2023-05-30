a615877fb9e0b26dda04dd83e5b118389bd6c2a4
Gui Silva <adrianon10@gmail.com>
2023-05-30 04:00:18 -0300

    style: add comma at the end

--- src/validations/create-memory-validation.ts
+++ src/validations/create-memory-validation.ts
@@ -3,5 +3,5 @@ import { z } from 'zod'
 export const createMemoryValidation = z.object({
   content: z.string(),
   coverUrl: z.string(),
-  isPublic: z.coerce.boolean().default(false)
+  isPublic: z.coerce.boolean().default(false),
 })
--- src/validations/update-memory-validation.ts
+++ src/validations/update-memory-validation.ts
@@ -3,5 +3,5 @@ import { z } from 'zod'
 export const updateMemoryValidation = z.object({
   content: z.string(),
   coverUrl: z.string(),
-  isPublic: z.coerce.boolean().default(false)
+  isPublic: z.coerce.boolean().default(false),
 })
