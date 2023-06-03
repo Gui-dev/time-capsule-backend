91b9f0d2661bde357ef0c5d2c5472184b83f0e60
Gui Silva <adrianon10@gmail.com>
2023-06-03 04:15:35 -0300

    feat: add checking user authentication

--- src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { type FastifyInstance } from 'fastify'
-
-export const appRoutes = async (app: FastifyInstance): Promise<void> => {
-  app.get('/', () => {
-    return 'Hello World'
-  })
-}
--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -9,15 +9,23 @@ import { createMemoryValidation } from '../validations/create-memory-validation'
 import { updateMemoryValidation } from '../validations/update-memory-validation'
 
 export const memoriesRoutes = async (app: FastifyInstance): Promise<void> => {
+  app.addHook('preHandler', async (request: FastifyRequest) => {
+    await request.jwtVerify()
+  })
+
   app.get(
     '/memories',
     async (request: FastifyRequest, response: FastifyReply) => {
+      const { sub } = request.user
       const memories = await prisma.memory.findMany({
+        where: {
+          userId: sub,
+        },
         orderBy: {
           createdAt: 'asc',
         },
       })
-      return await response.status(200).send(
+      return response.status(200).send(
         memories.map((memory) => {
           return {
             id: memory.id,
@@ -32,42 +40,64 @@ export const memoriesRoutes = async (app: FastifyInstance): Promise<void> => {
   app.get(
     '/memories/:id',
     async (request: FastifyRequest, response: FastifyReply) => {
+      const { sub } = request.user
       const { id } = memoriesParamsValidation.parse(request.params)
       const memory = await prisma.memory.findUniqueOrThrow({
-        where: { id },
+        where: {
+          id,
+        },
       })
-      return await response.status(200).send(memory)
+
+      if (!memory.isPublic && memory.userId !== sub) {
+        return response.status(401).send({ error: 'User unauthorized' })
+      }
+
+      return response.status(200).send(memory)
     },
   )
 
   app.post(
     '/memories',
     async (request: FastifyRequest, response: FastifyReply) => {
+      const { sub } = request.user
       const { content, coverUrl, isPublic } = createMemoryValidation.parse(
         request.body,
       )
       const memory = await prisma.memory.create({
         data: {
-          userId: '43443b1b-7bd1-4bfe-949f-9e43ea94756c',
+          userId: sub,
           content,
           coverUrl,
           isPublic,
         },
       })
 
-      return await response.status(201).send(memory)
+      return response.status(201).send(memory)
     },
   )
 
   app.put(
     '/memories/:id',
     async (request: FastifyRequest, response: FastifyReply) => {
+      const { sub } = request.user
       const { id } = memoriesParamsValidation.parse(request.params)
       const { content, coverUrl, isPublic } = updateMemoryValidation.parse(
         request.body,
       )
-      const memory = await prisma.memory.update({
-        where: { id },
+      let memory = await prisma.memory.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
+
+      if (memory.userId !== sub) {
+        return response.status(401).send({ error: 'User unauthorized' })
+      }
+
+      memory = await prisma.memory.update({
+        where: {
+          id,
+        },
         data: {
           content,
           coverUrl,
@@ -75,18 +105,29 @@ export const memoriesRoutes = async (app: FastifyInstance): Promise<void> => {
         },
       })
 
-      return await response.status(201).send(memory)
+      return response.status(201).send(memory)
     },
   )
 
   app.delete(
     '/memories/:id',
     async (request: FastifyRequest, response: FastifyReply) => {
+      const { sub } = request.user
       const { id } = memoriesParamsValidation.parse(request.params)
-      await prisma.memory.findUniqueOrThrow({
+      const memory = await prisma.memory.findUniqueOrThrow({
+        where: {
+          id,
+        },
+      })
+
+      if (memory.userId !== sub) {
+        return response.status(401).send({ error: 'User unauthorized' })
+      }
+
+      await prisma.memory.delete({
         where: { id },
       })
-      return await response.status(204).send()
+      return response.status(204).send()
     },
   )
 }
